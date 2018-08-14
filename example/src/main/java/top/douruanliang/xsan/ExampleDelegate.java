5e437e5c5c2b566b54931dd9a9e8dcdcb11afaf0
dourl <1141851728@qq.com>
2018-08-14 19:16:42 +0800

    网络

--- example/src/main/java/top/douruanliang/xsan/ExampleDelegate.java
+++ example/src/main/java/top/douruanliang/xsan/ExampleDelegate.java
@@ -5,6 +5,10 @@ import android.support.annotation.Nullable;
 import android.view.View;
 
 import top.douruanliang.xsan.core.delegate.XsanDelegate;
+import top.douruanliang.xsan.core.net.RestClient;
+import top.douruanliang.xsan.core.net.callback.IError;
+import top.douruanliang.xsan.core.net.callback.IFailure;
+import top.douruanliang.xsan.core.net.callback.ISuccess;
 
 /**
  * author: dourl
@@ -20,5 +24,30 @@ public class ExampleDelegate extends XsanDelegate {
     @Override
     public void onBindView(@Nullable Bundle saveInstanceState, View view) {
 
+    }
+
+    private void testRestClient() {
+        RestClient.builder()
+                .url("")
+                .params("", "")
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+
+                    }
+                })
+                .failure(new IFailure() {
+                    @Override
+                    public void onFailure() {
+
+                    }
+                })
+                .error(new IError() {
+                    @Override
+                    public void onError(int code, String msg) {
+
+                    }
+                })
+                .build();
     }
 }
